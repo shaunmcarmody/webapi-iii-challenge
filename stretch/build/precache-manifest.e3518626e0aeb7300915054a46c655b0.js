40c5d84863ecbf98265f5b8db6e9640f070625a2
c87 <shaun.carmody@live.co.uk>
2019-03-21 20:25:21 +0000

    build app

---
+++ stretch/build/precache-manifest.e3518626e0aeb7300915054a46c655b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab0e82b095aa7f6f4b0a",
+    "url": "/static/css/main.034b1bc7.chunk.css"
+  },
+  {
+    "revision": "ab0e82b095aa7f6f4b0a",
+    "url": "/static/js/main.a352d931.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0f72fc7a078031d2d3f1",
+    "url": "/static/js/2.8af8a7ce.chunk.js"
+  },
+  {
+    "revision": "f4f3d0c0bc7a7e8631c3e378a16d17e0",
+    "url": "/index.html"
+  }
+];
